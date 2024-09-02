66cc14330641047668e578ea0016236e08f99f90
kaneis1 <95732431+kaneis1@users.noreply.github.com>
2024-09-02 02:51:15 -0400

    Update hw0.py

--- hw0.py
+++ hw0.py
@@ -14,18 +14,18 @@ def draw_samples(n):
     return samples
   
 def sum_squares(N):
-    N=np.array(N,np.float64)
-    ans1=np.float64(0)
+    N=np.array(N,np.float128)
+    ans1=np.float128(0)
     for i in N:
         ans1+=i*i
     return ans1
 
 def troublemakers(n):
-    N=np.array([100000000.0000000000,100000000.0000000000],dtype=np.float64)
+    N=np.array([1.0,1.0],dtype=np.float64)
     for _ in range(n):
         
         N[1]+=0.35*N[0]
         N[0]-=0.35*N[0]
         N[0]+=N[1]*0.2
         N[1]-=0.2*N[1]
-    return N/100000000.0000000000
+    return N
