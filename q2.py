e5bc8fa4e917bfe846059bd6aa99650be6eef2cd
kaneis1 <95732431+kaneis1@users.noreply.github.com>
2024-09-23 00:02:37 -0400

    Update q2.py

--- q2.py
+++ q2.py
@@ -24,9 +24,7 @@ def split_features_target(data):
 
 def preprocess_data(trainx,valx,testx):
 
-    trainx = pd.DataFrame(trainx)
-    trainx = trainx.drop(columns=['Appliances','date'])
-    trainx=trainx.to_numpy().astype(int64)
+    trainx=trainx.astype(int64)
          
     return trainx,trainx,trainx
 
